ac119ff6e7610dbc464842ce08ddfb8184cf0bb7
Samuel Angulo <samuelangulo70@gmail.com>
2024-09-17 16:46:15 +0200

    menuFooterNavegador

---
+++ LibrodeRecetas/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
---
+++ LibrodeRecetas/src/Navegador.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Navegador() {
+  return (
+    <div>Navegador</div>
+  )
+}
+
+export default Navegador
