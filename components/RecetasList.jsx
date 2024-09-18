a22f40135dd0c54df4e811c24ec73309d1ddc653
Gemma Fernandez <gemmazuera@gmail.com>
2024-09-18 18:38:53 +0200

    day 2

---
+++ components/RecetasList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import recetas from "../components/recetas.json";
+import { useState } from "react";
+
+function RecetasList() {
+  const [listaRecetas, setListaRecetas] = useState(recetas);
+
+  function handleDelete(id) {
+    const arrayFiltrado = listaRecetas.filter( element => element.id !== id)
+    setListaRecetas(arrayFiltrado);
+  }
+  
+
+  return ( 
+      <ul>
+        {listaRecetas.map((cadaReceta, index) => (
+          <li key={cadaReceta.id} className="card-comida"> {/* Usar cadaReceta.id como key */}
+            <h1>{cadaReceta.name}</h1>
+            <img
+              src={cadaReceta.image}
+              alt="imagen comida"
+              style={{ width: "200px" }}
+            />
+            <h4>Calories: {cadaReceta.calories}</h4>
+            <h5>Servings: {cadaReceta.servings}</h5>
+  
+            {cadaReceta.calories >=  200 ? (
+              <p style={{ color: "red" }}>⚠️ High in calories</p>
+            ) : (
+              <p style={{ color: "green" }}>✅ Low in calories</p>
+            )}
+            
+            <button onClick={() => handleDelete(cadaReceta.id)} style={{ backgroundColor: "red" }}> 
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    )
+  }
+
+export default RecetasList;
--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./App.css";
-import Navegador from "./Navegador.jsx";
-
-import Sidebar from "./Sidebar.jsx";
-import Footer from "./Footer.jsx";
+import Navegador from "../components/Navegador.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+import Footer from "../components/Footer.jsx";
+import RecetasList from "../components/RecetasList.jsx"
 
 function App() {
   return (
     <>
       <Navegador />
-
       <Sidebar />
+      <RecetasList/>
       <Footer />
     </>
   );
